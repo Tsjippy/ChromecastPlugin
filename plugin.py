426c8251218bd2e1fe4514f4da31845b27d4117f
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2018-12-13 19:15:27 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -302,6 +302,7 @@ class BasePlugin:
 					if Text != "":
 						#Reset the variable to empty
 						requests.get(url=self.url+"/json.htm?type=command&param=updateuservariable&vname="+ChromecastName+"&vtype=2&vvalue=")
+						
 						if self.ConnectedChromecasts[ChromecastName][3] == "CONNECTED":
 							#Create mp3
 							os.system('curl -s -G "http://translate.google.com/translate_tts" --data "ie=UTF-8&total=1&idx=0&client=tw-ob&&tl='+self.Languague+'" --data-urlencode "q='+Text+'" -A "Mozilla" --compressed -o '+self.Filelocation+'/message.mp3')
@@ -412,6 +413,7 @@ class BasePlugin:
 						Domoticz.Log("Starting Youtube on chromecast")
 						yt = YouTubeController()
 						cc.register_handler(yt)
+				elif Unit % 10 == 5:
 			except Exception as e:
 				if str(e) == "Chromecast is connecting...":
 					pass
@@ -535,7 +537,7 @@ class BasePlugin:
 					senderror(e)
 
 				#Delete devices
-				for i in range(4):
+				for i in range(5):
 					x=deviceid+i
 					Domoticz.Log("Deleting '"+Devices[x].Name+"' with id "+str(x))
 					Devices[x].Delete()
@@ -626,6 +628,11 @@ def createDevices(Chromecasts):
 				Domoticz.Log("Created 'App' device for chromecast '"+Chromecast+"'")
 				Domoticz.Device(Name="App name-"+Chromecast, Unit=x+4, TypeName="Selector Switch", Switchtype=18, Options=_plugin.AppOptions, Used=1).Create()
 				UpdateImage(x+4, 'ChromecastLogo')
+
+			if x+5 not in Devices:
+				Domoticz.Log("Created 'Message' device for chromecast '"+Chromecast+"'")
+				Domoticz.Device(Name="Message-"+Chromecast, Unit=x+5, Type=243, Subtype=19, Used=1).Create()
+				UpdateImage(x+5, 'ChromecastLogo')
 
 		except Exception as e:
 			senderror(e)
